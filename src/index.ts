831d89ab6cde45b54017ac9ec23dcd17bea9c809
Clément Murzeau <clement@studiorelief.fr>
2024-06-24 18:56:16 +0200

    Last fix / remove window.webflow

--- src/index.ts
+++ src/index.ts
@@ -67,235 +67,230 @@ function resetWebflow(data: { next: { html: string } }): void {
   }
 }
 
-window.Webflow ||= [];
-(window.Webflow as (() => void)[]).push(() => {
-  loadAttributesScript(
-    'https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js'
-  );
+loadAttributesScript('https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js');
 
-  callWeglot();
-  loadModelViewerScript();
-  roundNumbersInClass();
-  document.addEventListener('DOMContentLoaded', hideElementsByOS);
+callWeglot();
+loadModelViewerScript();
+roundNumbersInClass();
+document.addEventListener('DOMContentLoaded', hideElementsByOS);
 
-  barba.hooks.leave(async (data) => {
-    if (!data) return;
-    await gsap.set(data.next.container, { opacity: 0 });
+barba.hooks.leave(async (data) => {
+  if (!data) return;
+  await gsap.set(data.next.container, { opacity: 0 });
+  setTimeout(() => {
+    window.scrollTo(0, 0);
     setTimeout(() => {
-      window.scrollTo(0, 0);
-      setTimeout(() => {
-        window.scrollBy(0, 1);
-      }, 500);
+      window.scrollBy(0, 1);
     }, 500);
-  });
+  }, 500);
+});
 
-  barba.hooks.enter(async (data) => {
-    if (!data) return;
-    callWeglot();
-    await gsap.to(data.next.container, {
-      opacity: 1,
-      duration: 0.5,
-    });
+barba.hooks.enter(async (data) => {
+  if (!data) return;
+  callWeglot();
+  await gsap.to(data.next.container, {
+    opacity: 1,
+    duration: 0.5,
   });
+});
 
-  barba.init({
-    preventRunning: true,
-    debug: true,
-    views: [
-      {
-        namespace: 'app',
-        beforeEnter() {
-          updateHomeHeroItem();
-          copyFitness();
-          initSwipelux();
-          getMobileOperatingSystem();
-          hideElementsByOS();
-        },
-        afterEnter() {
+barba.init({
+  preventRunning: true,
+  debug: true,
+  views: [
+    {
+      namespace: 'app',
+      beforeEnter() {
+        updateHomeHeroItem();
+        copyFitness();
+        initSwipelux();
+        getMobileOperatingSystem();
+        hideElementsByOS();
+      },
+      afterEnter() {
+        resetTimeline();
+        resetGsapHowIt();
+        heroAnim();
+      },
+      beforeLeave() {
+        resetTimeline();
+        resetGsapHowIt();
+      },
+    },
+    {
+      namespace: 'nft',
+      beforeEnter() {
+        loadTypedScript().then(() => {
+          nftTyping();
           resetTimeline();
           resetGsapHowIt();
-          heroAnim();
-        },
-        beforeLeave() {
-          resetTimeline();
-          resetGsapHowIt();
-        },
+        });
       },
-      {
-        namespace: 'nft',
-        beforeEnter() {
-          loadTypedScript().then(() => {
-            nftTyping();
-            resetTimeline();
-            resetGsapHowIt();
-          });
-        },
+    },
+    {
+      namespace: 'how-it-works',
+      beforeEnter() {
+        resetTimeline();
       },
-      {
-        namespace: 'how-it-works',
-        beforeEnter() {
-          resetTimeline();
-        },
-        afterEnter() {
-          const animations = [
-            heroHowIt,
-            createSlideHowIt,
-            mockupMoveHowIt,
-            rewardHowIt,
-            loadHowIt,
-            bagScaleHowIt,
-            babyfitScrollHowIt,
-            loadingSquadHowIt,
-            counterUpHowIt,
-            parallaxTeamHowIt,
-            parallaxJoinTeamHowIt,
-            parallaxTabsHowIt,
-          ];
+      afterEnter() {
+        const animations = [
+          heroHowIt,
+          createSlideHowIt,
+          mockupMoveHowIt,
+          rewardHowIt,
+          loadHowIt,
+          bagScaleHowIt,
+          babyfitScrollHowIt,
+          loadingSquadHowIt,
+          counterUpHowIt,
+          parallaxTeamHowIt,
+          parallaxJoinTeamHowIt,
+          parallaxTabsHowIt,
+        ];
+        setTimeout(() => {
+          Promise.all(animations.map((anim) => anim()));
+        }, 1000);
+      },
+      beforeLeave() {
+        resetTimeline();
+        resetGsapHowIt();
+      },
+    },
+    {
+      namespace: 'team',
+      beforeLeave() {
+        resetTimeline();
+        resetGsapHowIt();
+      },
+    },
+    {
+      namespace: 'legals',
+      beforeLeave() {
+        resetTimeline();
+        resetGsapHowIt();
+      },
+    },
+    {
+      namespace: 'blog',
+      beforeEnter() {
+        loadAttributesScript(
+          'https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js'
+        );
+      },
+      beforeLeave() {
+        resetTimeline();
+        resetGsapHowIt();
+      },
+    },
+  ],
+  transitions: [
+    {
+      name: 'app - left to right',
+      sync: true,
+      to: {
+        namespace: ['app'],
+      },
+      leave(data) {
+        if (!data) return;
+        const transitionData = data;
+        pTransAppRight(() => {
+          resetWebflow(transitionData);
+          window.scrollTo(0, 0);
           setTimeout(() => {
-            Promise.all(animations.map((anim) => anim()));
-          }, 1000);
-        },
-        beforeLeave() {
-          resetTimeline();
-          resetGsapHowIt();
-        },
+            window.scrollBy(0, 1);
+          }, 500);
+        });
       },
-      {
-        namespace: 'team',
-        beforeLeave() {
-          resetTimeline();
-          resetGsapHowIt();
-        },
+    },
+    {
+      name: 'nft - left to right',
+      sync: true,
+      to: {
+        namespace: ['nft'],
       },
-      {
-        namespace: 'legals',
-        beforeLeave() {
-          resetTimeline();
-          resetGsapHowIt();
-        },
+      leave(data) {
+        if (!data) return;
+        const transitionData = data;
+        pTransNftRight(() => {
+          resetWebflow(transitionData);
+          window.scrollTo(0, 0);
+          setTimeout(() => {
+            window.scrollBy(0, 1);
+          }, 500);
+        });
       },
-      {
-        namespace: 'blog',
-        beforeEnter() {
-          loadAttributesScript(
-            'https://cdn.jsdelivr.net/npm/@finsweet/attributes-cmsfilter@1/cmsfilter.js'
-          );
-        },
-        beforeLeave() {
-          resetTimeline();
-          resetGsapHowIt();
-        },
+    },
+    {
+      name: 'HIW - left to right',
+      sync: true,
+      to: {
+        namespace: ['how-it-works'],
       },
-    ],
-    transitions: [
-      {
-        name: 'app - left to right',
-        sync: true,
-        to: {
-          namespace: ['app'],
-        },
-        leave(data) {
-          if (!data) return;
-          const transitionData = data;
-          pTransAppRight(() => {
-            resetWebflow(transitionData);
-            window.scrollTo(0, 0);
-            setTimeout(() => {
-              window.scrollBy(0, 1);
-            }, 500);
-          });
-        },
+      leave(data) {
+        if (!data) return;
+        const transitionData = data;
+        pTransHiwRight(() => {
+          resetWebflow(transitionData);
+          window.scrollTo(0, 0);
+          setTimeout(() => {
+            window.scrollBy(0, 1);
+          }, 500);
+        });
       },
-      {
-        name: 'nft - left to right',
-        sync: true,
-        to: {
-          namespace: ['nft'],
-        },
-        leave(data) {
-          if (!data) return;
-          const transitionData = data;
-          pTransNftRight(() => {
-            resetWebflow(transitionData);
-            window.scrollTo(0, 0);
-            setTimeout(() => {
-              window.scrollBy(0, 1);
-            }, 500);
-          });
-        },
+    },
+    {
+      name: 'team - left to right',
+      sync: true,
+      to: {
+        namespace: ['team'],
       },
-      {
-        name: 'HIW - left to right',
-        sync: true,
-        to: {
-          namespace: ['how-it-works'],
-        },
-        leave(data) {
-          if (!data) return;
-          const transitionData = data;
-          pTransHiwRight(() => {
-            resetWebflow(transitionData);
-            window.scrollTo(0, 0);
-            setTimeout(() => {
-              window.scrollBy(0, 1);
-            }, 500);
-          });
-        },
+      leave(data) {
+        if (!data) return;
+        const transitionData = data;
+        pTransTeamRight(() => {
+          resetWebflow(transitionData);
+          window.scrollTo(0, 0);
+          setTimeout(() => {
+            window.scrollBy(0, 1);
+          }, 500);
+        });
       },
-      {
-        name: 'team - left to right',
-        sync: true,
-        to: {
-          namespace: ['team'],
-        },
-        leave(data) {
-          if (!data) return;
-          const transitionData = data;
-          pTransTeamRight(() => {
-            resetWebflow(transitionData);
-            window.scrollTo(0, 0);
-            setTimeout(() => {
-              window.scrollBy(0, 1);
-            }, 500);
-          });
-        },
+    },
+    {
+      name: 'legals - left to right',
+      sync: true,
+      to: {
+        namespace: ['legals'],
       },
-      {
-        name: 'legals - left to right',
-        sync: true,
-        to: {
-          namespace: ['legals'],
-        },
-        leave(data) {
-          if (!data) return;
-          const transitionData = data;
-          pTransLegalsRight(() => {
-            resetWebflow(transitionData);
-            window.scrollTo(0, 0);
-            setTimeout(() => {
-              window.scrollBy(0, 1);
-            }, 500);
-          });
-        },
+      leave(data) {
+        if (!data) return;
+        const transitionData = data;
+        pTransLegalsRight(() => {
+          resetWebflow(transitionData);
+          window.scrollTo(0, 0);
+          setTimeout(() => {
+            window.scrollBy(0, 1);
+          }, 500);
+        });
       },
-      {
-        name: 'blog - left to right',
-        sync: true,
-        to: {
-          namespace: ['blog'],
-        },
-        leave(data) {
-          if (!data) return;
-          const transitionData = data;
-          pTransBlogRight(() => {
-            resetWebflow(transitionData);
-            window.scrollTo(0, 0);
-            setTimeout(() => {
-              window.scrollBy(0, 1);
-            }, 500);
-          });
-        },
+    },
+    {
+      name: 'blog - left to right',
+      sync: true,
+      to: {
+        namespace: ['blog'],
       },
-    ],
-  });
+      leave(data) {
+        if (!data) return;
+        const transitionData = data;
+        pTransBlogRight(() => {
+          resetWebflow(transitionData);
+          window.scrollTo(0, 0);
+          setTimeout(() => {
+            window.scrollBy(0, 1);
+          }, 500);
+        });
+      },
+    },
+  ],
 });
